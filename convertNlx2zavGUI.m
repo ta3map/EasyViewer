b1fd273e60e84a7b321bd1b48698101bfc0ea30e
Azat Gainutdinov <ta3map@gmail.com>
2024-03-22 18:25:21 +0100

    version 1.07

--- convertNlx2zavGUI.m
+++ convertNlx2zavGUI.m
@@ -8,10 +8,12 @@ function convertNlx2zavGUI
         active_folder = userpath;
     end
             
-    persistent recordPath detectMua hWaitBar channels_n lfp hd lfp_Fs orig_Fs spks mua_std_coef
+    persistent recordPath detectMua hWaitBar channels_n lfp hd orig_Fs spks mua_std_coef
+    persistent lfp_Fs channels_list
     
     % Порог MUA
     mua_std_coef = 3;
+    lfp_Fs = 1000;
     
     % Создаем главное окно GUI
     fig = figure('Name', 'Convert to ZAV', 'Position', [100, 100, 280, 200], 'NumberTitle', 'off',...
@@ -33,9 +35,24 @@ function convertNlx2zavGUI
     muaCoefUIcoords = [208, 100, 20, 25];
     muaCoefUI = uicontrol('Style', 'edit', 'String', num2str(mua_std_coef), 'Position', muaCoefUIcoords, 'Callback', @muaCoefUICallback);
     
+    % Окошко для значения частоты дикретизации lfp   
+    lfpFsTitlecoords = [160, 45, 50, 25];
+    lfpFsTitleUI = uicontrol('Style', 'text', 'String', 'Fs, Hz', 'Position', lfpFsTitlecoords);
+    
+    lfpFsUIcoords = [208, 50, 60, 25];
+    lfpFsUI = uicontrol('Style', 'edit', 'String', num2str(lfp_Fs), 'Position', lfpFsUIcoords, 'Callback', @lfpFsUICallback);
+    
+    uicontrol('Style', 'text', ...
+          'Position', [10 65 120 25], ...
+          'String', 'Chosen channels:');
+    h = uicontrol('Style', 'edit', ...
+              'Position', [10 50 120 25], ...
+              'String', 'all channels', ...
+              'Callback', @chosenChannelsCallback);
+    
     % Кнопка для запуска конвертации
     uicontrol('Style', 'pushbutton', 'String', 'Start Conversion', ...
-        'Position', [50, 50, 150, 25], 'Callback', @startConversion);
+        'Position', [50, 10, 150, 25], 'Callback', @startConversion);
     
     % Глобальные переменные
     recordPath = '';
@@ -44,6 +61,10 @@ function convertNlx2zavGUI
     
     function muaCoefUICallback(source, ~)
         mua_std_coef = str2double(get(source, 'String'));
+    end
+
+    function lfpFsUICallback(source, ~)
+        lfp_Fs = str2double(get(source, 'String'));
     end
 
     % Функция обратного вызова для выбора пути к записи
@@ -57,6 +78,37 @@ function convertNlx2zavGUI
         end
     end
     
+    function chosenChannelsCallback(hObject, ~)
+        
+        if isempty(recordPath)
+            warndlg('Please select a record path first.', 'Warning');
+            return;
+        end
+        
+        channels_n = countChannels(recordPath);  % number of channels
+        disp(['channels total: ' num2str(channels_n)]);
+        
+        val = get(hObject, 'String');
+
+        % Проверка, если введено 'all channels'
+        if strcmp(val, 'all channels')
+            channels_list = 1:channels_n;
+            disp('all channels chosen')
+        else
+            % Пытаемся интерпретировать введенное значение как диапазон или список каналов
+            try
+                channels_list = eval(['[', val, ']']);
+            catch
+                % В случае ошибки возвращаем пустой массив и выводим сообщение об ошибке
+                channels_list = [];
+                disp('Invalid input. Please enter channels like "1,2,3" or "1:5".');
+            end
+        end
+
+        % Демонстрация полученного списка каналов (можно убрать или заменить на другое действие)
+        disp(['channel list: ' num2str(channels_list)]);
+    end
+
     % Функция обратного вызова для запуска конвертации
     function startConversion(~, ~)
         detectMua = get(detectMuaToggle, 'Value');
@@ -67,10 +119,13 @@ function convertNlx2zavGUI
         
         disp(['Starting conversion. Detect MUA: ', num2str(detectMua)]);
         
-        channels_n = countChannels(recordPath);  % number of channels
-        channels_list = 1:channels_n;
-
-        lfp_Fs = 1000; % новая частота
+        chosenChannelsCallback(h, [])
+        
+        channels_n = numel(channels_list);
+        
+        disp(['channels total: ' num2str(channels_n)]);
+        disp(['channel list: ' num2str(channels_list)]);
+        
         lfp = []; % инициализация переменной для lfp
 
         % Создание окна прогресса
@@ -83,32 +138,45 @@ function convertNlx2zavGUI
         lfp = zeros(lfp_length, channels_n); % предварительное выделение памяти для lfp
 
         clear spks
-
+        ch_inx = 0;
         for ch = channels_list
+            ch_inx = ch_inx+1;
+            
             [data, ~, hd, spkTS, spkSM] = ZavNrlynx2(recordPath, [], ch, [], []);
 
             ampl = min(spkSM.s(:, :));
 
             if detectMua
                 [tStamp, ampl, shape] = detectMUA(data, hd, mua_std_coef, true);
-                spks(ch).tStamp = single(tStamp); % сохраняем спайки канала в миллисекундном формате
-                spks(ch).ampl = single(ampl);
-                spks(ch).shape = shape;
+                spks(ch_inx).tStamp = single(tStamp); % сохраняем спайки канала в миллисекундном формате
+                spks(ch_inx).ampl = single(ampl);
+                spks(ch_inx).shape = shape;
             else
                 % по ZAV формату
-                spks(ch).tStamp = single(spkTS.s'); % сохраняем спайки канала в миллисекундном формате
-                spks(ch).ampl = single(ampl');
-                spks(ch).shape = [];
+                spks(ch_inx).tStamp = single(spkTS.s'); % сохраняем спайки канала в миллисекундном формате
+                spks(ch_inx).ampl = single(ampl');
+                spks(ch_inx).shape = [];
             end
 
             % Ресамплинг данных канала
             data_resampled = resample(data, lfp_Fs, orig_Fs);
-            lfp(:, ch) = data_resampled; % добавляем ресемплированные данные в матрицу lfp
+            lfp(:, ch_inx) = data_resampled; % добавляем ресемплированные данные в матрицу lfp
 
             % Обновление индикатора прогресса
-            waitbar(ch / numel(channels_list), hWaitBar, sprintf('Channel %d from %d...', ch, numel(channels_list)));
+            waitbar(ch_inx / numel(channels_list), hWaitBar, sprintf('Channel %d from %d...', ch, numel(channels_list)));
         end
-
+        
+        % преобразуем заголовок для выбранного списка каналов
+        hd.adBitVolts = hd.adBitVolts(channels_list);
+        hd.dspDelay_mks = hd.dspDelay_mks(channels_list);
+        hd.adBitVoltsSpk = hd.adBitVoltsSpk(channels_list);
+        hd.dspDelay_mksSpk = hd.dspDelay_mksSpk(channels_list);
+        hd.alignmentPt = hd.alignmentPt(channels_list);
+        hd.inverted = hd.inverted(channels_list);
+        hd.recChUnits = hd.recChUnits(channels_list); 
+        hd.recChNames = hd.recChNames(channels_list);
+        hd.ch_si = hd.ch_si(channels_list);
+        
         close(hWaitBar); % Закрытие окна прогресса
         disp('over')
 
